9f8f4acaff68206766b7d3c05847429616be6baf
Binod Kafle <mebinod50@gmail.com>
2025-04-29 13:55:45 +0545

    :sparkles: chore: integrate LinkedIn and Twitter data for enhanced profile summaries

--- basics.py
+++ basics.py
@@ -1,26 +1,48 @@
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 from dotenv import load_dotenv
-load_dotenv()
 
-if __name__ == "__main__":
-    print("Langchain Basics")
+from agents.linkedin_lookup_agent import linkedin_lookup_agent
+from agents.twitter_lookup_agent import twitter_lookup_agent
+from scrapers.linkedin import scrape_linkedin_profile
+from output_parsers import summary_parser
+from scrapers.twitter import scrape_user_tweets
+
+
+def ice_break_with(name: str) -> None:
+    """Returns a greeting message"""
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username)
 
     summary_template = """
-        give the information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+        given the information about a person from linkedin {linkedin_data},
+        and their latest twitter posts {twitter_posts} I want you to create:
+        1. A short summary
+        2. two interesting facts about them 
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+        Use both information from twitter and Linkedin
+        \n{format_instructions}
+        """
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="llama3.2")
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
+    )
 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
 
-    res = chain.invoke(input={"information": "Elon Musk"})
+    chain = summary_prompt_template | llm | summary_parser
+
+    res = chain.invoke(input={"linkedin_data": linkedin_data, "twitter_posts": tweets})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    ice_break_with(name="Binod Kafle")
