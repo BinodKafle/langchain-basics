cb2ed170175413c76d77006cbdfc632c0c3a2ce3
Binod Kafle <mebinod50@gmail.com>
2025-04-29 16:53:33 +0545

    :sparkles: feat: refactor information gathering function and update dependencies for enhanced functionality

--- basics.py
+++ basics.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -6,11 +8,11 @@ from dotenv import load_dotenv
 from agents.linkedin_lookup_agent import linkedin_lookup_agent
 from agents.twitter_lookup_agent import twitter_lookup_agent
 from scrapers.linkedin import scrape_linkedin_profile
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 from scrapers.twitter import scrape_user_tweets
 
 
-def gather_information(name: str) -> None:
+def finder(name: str) -> Tuple[Summary, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
 
@@ -30,18 +32,21 @@ def gather_information(name: str) -> None:
     summary_prompt_template = PromptTemplate(
         input_variables=["information", "twitter_posts"],
         template=summary_template,
-        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
-    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
     chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"linkedin_data": linkedin_data, "twitter_posts": tweets})
-
-    print(res)
+    res: Summary = chain.invoke(
+        input={"linkedin_data": linkedin_data, "twitter_posts": tweets}
+    )
+    return res, linkedin_data.get("photoUrl")
 
 
 if __name__ == "__main__":
     load_dotenv()
-    gather_information(name="Binod Kafle")
+    finder(name="Binod Kafle")
